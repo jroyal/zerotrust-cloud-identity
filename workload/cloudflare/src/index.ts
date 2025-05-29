e41ed817eab4178db0929090d6ecaa03cf67cca9
James Royal <jroyal@cloudflare.com>
2025-05-29 10:43:08 +0100

    Add basic worker that does most of what the example express app did

--- workload/cloudflare/src/index.ts
+++ workload/cloudflare/src/index.ts
@@ -10,11 +10,89 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-import { Hono } from 'hono';
-const app = new Hono();
+import { Context, Hono } from 'hono';
+import { parse } from 'yaml';
 
-app.get('/', (c) => c.text('Hono!'));
+type Bindings = {
+	CONFIG: KVNamespace;
+};
+
+const WORKLOAD_CONFIG_KEY = 'workload_config';
+
+export interface Workload {
+	provider: string;
+	host: string;
+	type: string;
+}
+// Define type for hosts configuration
+export interface HostsConfig {
+	[workloadName: string]: Workload;
+}
+
+// Define type for provider functions
+type ProviderFunction = (c: Context<{ Bindings: Bindings }>) => Promise<Response>;
+
+// Define type for providers object
+interface ProviderMap {
+	[providerName: string]: ProviderFunction;
+}
+
+const Providers: ProviderMap = {
+	example: async (c: Context<{ Bindings: Bindings }>) => {
+		return await forwardRequest(c);
+	},
+};
+
+const getUserAndHistory = async (c: Context<{ Bindings: Bindings }>) => {
+	return c.json({
+		user: 'unknown user',
+		history: ['insert identity history0 here', 'insert identity history1 here'],
+	});
+};
+
+const refreshConfig = async (env: Env) => {
+	const resp = await fetch('https://raw.githubusercontent.com/co-cddo/zerotrust-cloud-identity/refs/heads/main/shared_config/hosts.yaml');
+	const yamlData = await resp.text();
+	const jsonData = await parse(yamlData);
+	await env.CONFIG.put(WORKLOAD_CONFIG_KEY, JSON.stringify(jsonData));
+	return jsonData;
+};
+
+async function forwardRequest(c: Context<{ Bindings: Bindings }>) {
+	return new Response('heyo');
+}
+
+async function handleRequest(request: Request<unknown, CfProperties<unknown>>, env: Env, ctx: any) {
+	let workloadConfig: HostsConfig | null = await env.CONFIG.get(WORKLOAD_CONFIG_KEY, 'json');
+	if (!workloadConfig) {
+		workloadConfig = await refreshConfig(env);
+	}
+	const app = new Hono<{ Bindings: Bindings }>();
+	for (const workloadName in workloadConfig) {
+		console.log(`Setting up routes for workload: ${workloadName}`);
+		const workload = workloadConfig[workloadName];
+		const provider = Providers[workload.provider];
+		if (provider) {
+			app.get(`/${workloadName}`, provider);
+			app.get(`/${workloadName}/*`, provider);
+			app.post(`/${workloadName}/*`, provider);
+			app.put(`/${workloadName}/*`, provider);
+			app.delete(`/${workloadName}/*`, provider);
+			app.patch(`/${workloadName}/*`, provider);
+			console.log(`Routes registered for ${workloadName} with provider: ${workload.provider}`);
+		} else {
+			console.error(`Provider '${workload.provider}' not found for workload: ${workloadName}`);
+		}
+	}
+
+	app.get('/config', async (c) => {
+		const config = await refreshConfig(c.env);
+		return c.json(config);
+	});
+	app.get('*', (c) => getUserAndHistory(c));
+	return app.fetch(request);
+}
 
 export default {
-	fetch: app.fetch,
+	fetch: handleRequest,
 } satisfies ExportedHandler<Env>;
